d1fda238b74a322632e44897d49ba1aa3798486c
jorge-573 <100395309+jorge-573@users.noreply.github.com>
2022-04-20 17:00:05 -0500

    Update Main.java
    Load Main.fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,31 @@
 package application;
+	
+import java.io.File;
+import java.net.URL;
 
-public class Main {
-	public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			URL url = new File("src/Main.fxml").toURI().toURL();
+			AnchorPane root = FXMLLoader.load(url);
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
