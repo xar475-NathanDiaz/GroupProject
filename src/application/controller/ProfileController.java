fb94de66ac1c4ec451494ee9f720177a2da15925
jorge-573 <100395309+jorge-573@users.noreply.github.com>
2022-04-21 15:28:39 -0500

    Update ProfileController.java
    Added Main Menu Button, handleMainMenu(), and anchor pane fxid (anchor). handleMainMenu should give functionality to main menu button to go back to main menu.

--- src/application/controller/ProfileController.java
+++ src/application/controller/ProfileController.java
@@ -1,12 +1,25 @@
 package application.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class ProfileController {
 
+	@FXML
+	private AnchorPane anchor;
+	 
 	@FXML
 	private TextField nameField;
 
@@ -21,5 +34,17 @@ public class ProfileController {
 
 	@FXML
 	private ListView<?> scheduleList;
+	
+	@FXML
+   	private Button MainMenuButton;
+
+    	@FXML
+    	void handelMainMenu(ActionEvent event) throws IOException {
+    		URL url = new File("src/application/view/Main.fxml").toURI().toURL();
+		anchor = FXMLLoader.load(url);
+		Scene scene = new Scene(anchor);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+    }
 
 }
