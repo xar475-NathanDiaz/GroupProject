dd1f9ae87e293cf2804b4c3980084d571b4e52c4
Nathan Diaz <Nathan.Diaz@my.utsa.edu>
2022-04-21 17:15:50 -0500

    MainController now opens profile and schedule

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -1,10 +1,20 @@
 package application.controller;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-public class MainController {
+public class MainController implements Initializable{
 
 	@FXML
 	private Button createScheduleBttn;
@@ -22,12 +32,37 @@ public class MainController {
 
 	@FXML
 	void openProfile(ActionEvent event) {
-
+		try {
+			URL url = new File("src/application/view/Profile.fxml").toURI().toURL();
+			Parent root = FXMLLoader.load(url);
+			Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Scene scene = new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			window.setScene(scene);
+			window.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@FXML
 	void openSchedule(ActionEvent event) {
+		try {
+			URL url = new File("src/application/view/Schedule.fxml").toURI().toURL();
+			Parent root = FXMLLoader.load(url);
+			Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Scene scene = new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			window.setScene(scene);
+			window.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
 	}
 
 }
