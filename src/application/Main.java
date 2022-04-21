db1a003ea9f9abb1993d25f076a20254ad5c2393
Nathan Diaz <Nathan.Diaz@my.utsa.edu>
2022-04-20 21:25:02 -0500

    Properly opens Main.fxml now

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,9 +4,10 @@ import java.io.File;
 import java.net.URL;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -14,14 +15,15 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			URL url = new File("src/Main.fxml").toURI().toURL();
-			AnchorPane root = FXMLLoader.load(url);
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			URL url = new File("src/application/view/Main.fxml").toURI().toURL();
+			Parent root = FXMLLoader.load(url);
+			Scene scene = new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
+			Platform.exit();
 		}
 	}
 	
