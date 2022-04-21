7429e09c3775cb60b65e513378603efbd128e3a3
Nathan Diaz <Nathan.Diaz@my.utsa.edu>
2022-04-21 17:33:23 -0500

    Added files for a menu of all program files

---
+++ src/application/controller/BrowserController.java
@@ -0,0 +1,29 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class BrowserController {
+
+    @FXML
+    private Button backBttn;
+
+    @FXML
+    private ListView<?> itemList;
+
+    @FXML
+    private Button openBttn;
+
+    @FXML
+    void openItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void prevMenu(ActionEvent event) {
+
+    }
+
+}
---
+++ src/application/model/BrowserData.java
@@ -0,0 +1,8 @@
+package application.model;
+
+public class BrowserData {
+    /**
+     * this is a int that represents the previous menu for the back button
+     */
+    static int prevMenu;
+}
